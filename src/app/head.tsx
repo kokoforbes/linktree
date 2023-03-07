797151e0d55845557e600d389da18e58c0afef10
gsm-abdullahi <onikokoabdullahi@gmail.com>
2023-03-07 12:21:42 +0100

    Refactored pages

---
+++ src/app/head.tsx
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <title>Linktree</title>
+      <meta name='description' content='Next.js + Tailwind CSS' />
+      <link rel='shortcut icon' href='favicon.ico' type='image/x-icon' />
+      <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+    </>
+  );
+}
